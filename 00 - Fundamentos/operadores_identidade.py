9a090211c3072607aa63ef0b270c2405df19ba01
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-06-29 14:25:07 -0300

    upadated operadores de identidade

--- 00 - Fundamentos/operadores_identidade.py
+++ 00 - Fundamentos/operadores_identidade.py
@@ -1,5 +1,14 @@
-saldo = 1000
-limite = 1000
+#comparam se os dois objetos testadam ocupam a mesma posição na memória
+curso = "Curso de Python"
+nome_curso = curso
+saldo, limite = 1000
+
+curso is nome_curso #True
+curso is not nome_curso #False
+saldo is limite #True
+saldo is not limite #False
 
 print(saldo is limite)
 print(saldo is not limite)
+print(curso is not nome_curso)
+print(curso is nome_curso)
