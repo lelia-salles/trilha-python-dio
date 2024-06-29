46e7b60e5efb516a41b9870a2d1f963013a6171b
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-06-29 14:34:10 -0300

    updated operadores associação

--- 00 - Fundamentos/operadores_associacao.py
+++ 00 - Fundamentos/operadores_associacao.py
@@ -1,6 +1,15 @@
-frutas = ["limao", "uva"]
+# verificam se um objeto está presente em uma seguência
+frutas = ["limao", "uva", "laranja"]
 curso = "Curso de python"
+saques = [1500, 100]
+
+# in verifica se está na sequência e in not verifica se não está na sequência
+
+"python" in curso # True
+"maçã" not in frutas # True
+200 in saques # Flase 
 
 print("laranja" not in frutas)
 print("limao" in frutas)
 print("Python" in curso)
+print(200 in saques)
